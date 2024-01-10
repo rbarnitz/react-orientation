4548d6aeb33a88c2cc597bbc0a1d30d4808feeda
Richard Barnitz <richardbarnitz@gmail.com>
2024-01-09 17:35:58 -0800

    created Card and Footer JSX components

---
+++ src/components/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+function Card(){
+
+return(
+<div className="card">
+        <button>
+          count is 0
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+);
+}
+
+export default Card;
---
+++ src/components/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+return (
+        <p className="read-the-docs">
+            Practicing with React Footer JSX
+      </p>
+);
+}
+
+export default Footer;
